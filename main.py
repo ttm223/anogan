9a90b9996b453c3f15513e197ad3ce8700e7686b
ttm223 <taks.sgm@gmail.com>
2018-07-20 19:09:05 +0900

    Interim

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import anogan
 def main():
     yaml_path = input('input yaml file path: ')
     print('get path: {}'.format(yaml_path))
-    ano = anogan.anoGAN
+    ano = anogan.anoGAN()
     _, _ = ano.train(yaml_path=yaml_path)
 
 if __name__ == '__main__':
