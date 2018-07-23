bd27683cd5ec24afd623b984c24c620c01871c31
ttm223 <taks.sgm@gmail.com>
2018-07-23 21:55:09 +0900

    now debug-ing

--- anogan.py
+++ anogan.py
@@ -333,10 +333,8 @@ class anoGAN(object):
 
                 progress_bar.update(idx, values=[('g-loss', g_loss), ('d-loss', d_loss)])
                 if idx == n_iter - 1:
-                    np.save(join(self.save_dir, 'real_e{}.npy'.format(ep)),
-                            real_img)
-                    np.save(join(self.save_dir, 'fake_e{}.npy'.format(ep)),
-                            fake_img)
+                    np.save(join(self.save_dir, 'e{}.npy'.format(ep)), X)
+                    
             print('')
             # save weights per epoch for test
             g.save_weights(join(self.save_dir, 'g_weights_e{}.h5'.format(ep)), True)
